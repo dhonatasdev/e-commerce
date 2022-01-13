dfd39d731d4e1e64c8353e3e8ef228be3cbedca5
José Dhonatas Alves Sales <dhonatas.dev@gmail.com>
2022-01-13 20:14:55 -0300

    j

---
+++ e-commerce/Models/Ator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace e_commerce.Models
+{
+    public class Ator
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FotoLink { get; set; }
+        public string NomeCompleto { get;set }
+        public string Bio { get;set }
+    }
+}
---
+++ e-commerce/Models/Cinema.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace e_commerce.Models
+{
+    public class Cinema
+    {
+        [Key]
+        public string Logo { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+    }
+}
